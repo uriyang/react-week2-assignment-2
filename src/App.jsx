5b8e74f0e0007eead660da2ff55f381b41604872
uriyang <girlstoryk@gmail.com>
2021-12-19 21:52:08 +0900

    add: App file
    - add development mode

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Input() {
+  return null;
+}
+
+function List() {
+  return null;
+}
+
+export default function App() {
+  return (
+    <div>
+      <h1>To-do</h1>
+      <Input />
+      <List />
+    </div>
+  );
+}
